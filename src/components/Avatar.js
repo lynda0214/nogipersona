548614c8f8a6915f7dc1d288f3e628476849fb04
Lynda Lin <lynda0214@gmail.com>
2023-06-08 18:59:27 +0800

    add placeholder space for loading images without cumulative layout shift

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -9,8 +9,8 @@ const Avatar = ({ id, name }) => {
 
   return (
     <div className="w-28 md:w-44 cursor-pointer" onClick={clickHandler}>
-      <div className="w-24 h-24 md:w-40 md:h-40 rounded-full overflow-hidden mx-auto">
-        <img className="hover:scale-110" src={`/images/${id}_prof.jpg`} alt="" />
+      <div className="relative w-24 h-24 md:w-40 md:h-40 rounded-full overflow-hidden mx-auto bg-slate-200">
+        <img className="absolute left-0 top-0 hover:scale-110" src={`/images/${id}_prof.jpg`} alt="" />
       </div>
       <h1 className="font-noto mt-1 text-center">{name}</h1>
     </div>
--- src/components/MemberGradeTile.js
+++ src/components/MemberGradeTile.js
@@ -9,11 +9,12 @@ const MemberGradeTile = ({ grade }) => {
 
   return (
     <div
-      className="cursor-pointer bg-purple-600 hover:bg-purple-800 rounded overflow-hidden"
+      className="relative cursor-pointer bg-purple-600 hover:bg-purple-800 rounded overflow-hidden"
+      style={{ paddingTop: '66.6%' }}
       onClick={clickHandler}
     >
-      <img className="hover:grayscale hover:mix-blend-screen" src={`/images/${grade}.jpg`} alt="" />
-      <h1 className="font-noto text-white font-extrabold	text-end pr-4" style={{ fontSize: '3rem' }}># {grade}  期生</h1>
+      <img className="absolute left-0 top-0 hover:grayscale hover:mix-blend-screen" src={`/images/${grade}.jpg`} alt="" />
+      <h1 className="font-noto text-white font-extrabold text-end pr-4" style={{ fontSize: '3rem' }}># {grade}  期生</h1>
     </div>
   );
 };
