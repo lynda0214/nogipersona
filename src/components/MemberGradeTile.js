8fef0461c35e8a6dd1038ccd8c65f18aebd2b1a5
Lynda Lin <lynda0214@gmail.com>
2023-06-08 09:31:56 +0800

    change homepage style add <base>

--- src/components/MemberGradeTile.js
+++ src/components/MemberGradeTile.js
@@ -12,7 +12,7 @@ const MemberGradeTile = ({ grade }) => {
       className="cursor-pointer bg-purple-600 hover:bg-purple-800 rounded overflow-hidden"
       onClick={clickHandler}
     >
-      <img className="mix-blend-screen grayscale" src={`/nogipersona/images/${grade}.jpg`} alt="" />
+      <img className="hover:grayscale hover:mix-blend-screen" src={`/nogipersona/images/${grade}.jpg`} alt="" />
       <h1 className="font-noto text-white font-extrabold	text-end pr-4" style={{ fontSize: '3rem' }}># {grade}  期生</h1>
     </div>
   );
